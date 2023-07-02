0ad9eb36cbe30d334bec3d0151e54c418a4cf3fc
Mrutyunjay Madarimat <mrutyunjay69@gmail.com>
2023-07-03 03:03:59 +0530

    commit till firebase

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import {ReactComponent as WebLogo} from '../../assets/crown.svg';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                  <WebLogo className='logo' />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className='nav-link' to='/shop' style={{color:'black'}}>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation;
